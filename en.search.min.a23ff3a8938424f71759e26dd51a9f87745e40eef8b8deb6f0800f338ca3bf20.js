56c4fe711fd8baae5f55e9960580c0f77f0d5528
yewang <yewang@baidu.com>
2021-02-08 11:40:08 +0800

    rebuilding site Mon Feb  8 11:40:03 CST 2021

--- en.search.min.a23ff3a8938424f71759e26dd51a9f87745e40eef8b8deb6f0800f338ca3bf20.js
+++ en.search.min.a23ff3a8938424f71759e26dd51a9f87745e40eef8b8deb6f0800f338ca3bf20.js
@@ -3,7 +3,7 @@ input.addEventListener('focus',init);input.addEventListener('keyup',search);docu
 const characterPressed=String.fromCharCode(event.charCode);if(!isHotkey(characterPressed)){return;}
 input.focus();event.preventDefault();}
 function isHotkey(character){const dataHotkeys=input.getAttribute('data-hotkeys')||'';return dataHotkeys.indexOf(character)>=0;}
-function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/study-kubernetes/flexsearch.min.js');loadScript('/study-kubernetes/en.search-data.min.3416e41facd8417c919ad53e53411567ecb055479e15e0ae07b7f495fbbb8f7a.js',function(){input.required=false;search();});}
+function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/study-kubernetes/flexsearch.min.js');loadScript('/study-kubernetes/en.search-data.min.50f51ea531bf7d73072e2ef69088abe17833b30e8116c3be25597c63c278eb37.js',function(){input.required=false;search();});}
 function search(){while(results.firstChild){results.removeChild(results.firstChild);}
 if(!input.value){return;}
 const searchHits=window.bookSearchIndex.search(input.value,10);searchHits.forEach(function(page){const li=element('<li><a href></a><small></small></li>');const a=li.querySelector('a'),small=li.querySelector('small');a.href=page.href;a.textContent=page.title;small.textContent=page.section;results.appendChild(li);});}
